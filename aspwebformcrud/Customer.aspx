357f9b8d140ab5f230e306ebeb3e53339aeae2bb
witzelsuchter <mrktgle@gmail.com>
2019-06-23 11:20:03 +0800

    Added Files

---
+++ aspwebformcrud/Customer.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Customer.aspx.cs" Inherits="aspwebformcrud.Customer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:HiddenField ID="hfCustomerID" runat="server" />
+        <table>
+            <tr> 
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>   
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="City"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+                </td>   
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                </td>   
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+                </td>   
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+                </td>   
+            </tr>
+        </table>
+        <br />
+        <asp:GridView ID="gvCustomer" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Name" />
+                <asp:BoundField DataField="City" HeaderText="City" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkView" runat="server" CommandArgument='<% # Eval("CustomerID") %>' OnClick="Link_Onlick">View</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ aspwebformcrud/Customer.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspwebformcrud
+{
+    public partial class Customer : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Server=localhost; Database=ASPCRUD; Integrated Security=SSPI;");
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataTable table;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillDataGrid();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void FillDataGrid()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                da = new SqlDataAdapter("CustomerViewAll", conn);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                table = new DataTable();
+                da.Fill(table);
+                conn.Close();
+                gvCustomer.DataSource = table;
+                gvCustomer.DataBind();
+
+            }
+        }
+
+        public void Clear()
+        {
+            hfCustomerID.Value = "";
+            txtName.Text = txtCity.Text = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed) {
+                conn.Open();
+                cmd = new SqlCommand("CustomerCreateOrUpdate",conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerID", (hfCustomerID.Value == "" ? 0 : Convert.ToInt32(hfCustomerID.Value)));
+                cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                cmd.Parameters.AddWithValue("@City", txtCity.Text.Trim());
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                string CustomerID = hfCustomerID.Value;
+                if (CustomerID == "")
+                {
+                    Clear();
+                    lblSuccessMessage.Text = "Save Successfully";
+                    FillDataGrid();
+                }
+                else {
+                    Clear();
+                    lblSuccessMessage.Text = "Update Successfully";
+                    FillDataGrid();
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                cmd = new SqlCommand("CustomerDeleteByID", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerID", Convert.ToInt32(hfCustomerID.Value));
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                FillDataGrid();
+                Clear();
+                lblSuccessMessage.Text = "Delete Successfully";
+            }
+        }
+
+        protected void Link_Onlick(object sender, EventArgs e)
+        {
+            int CustomerID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                da = new SqlDataAdapter("CustomerViewByID", conn);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@CustomerID", CustomerID);
+                table = new DataTable();
+                da.Fill(table);
+                conn.Close();
+                hfCustomerID.Value = CustomerID.ToString();
+                txtName.Text = table.Rows[0]["Name"].ToString();
+                txtCity.Text = table.Rows[0]["City"].ToString();
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+    }
+}
